2f96ae5939c78ff3c06eaa271742aac182eab0f7
Conner Bachmann <github@conner-bachmann.de>
2023-02-21 11:47:10 +0100

    feat: initial commit

---
+++ pages/packages/[slug].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Dynamic NuxtPage
+    Slug: {{  slug }}
+  </div>
+</template>
+
+<script setup>
+const availableSlugs = ["test", "nuxt", "nuxt2"]
+const route = useRoute();
+const slug = route.params.slug;
+
+// if (!availableSlugs.includes(slug)) {
+//   throw createError({ statusCode: 404, statusMessage: "Not found"})
+// }
+</script>
